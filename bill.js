3418540ad4593cf5f99e0891fd9976e634fbf757
Juti <jbuican19@gmail.com>
2021-08-01 22:31:40 +0100

    Update bill.js
    Created a separate function to do the calculation, 
    remove local variable

--- bill.js
+++ bill.js
@@ -1,52 +1,58 @@
-module.exports = function statement(invoice, plays) {
+module.exports = function statement(invoice, plays) {  console.log(`data :: \n`);console.log(invoice)
+ console.log(plays); console.log(' ')
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `statement for ${invoice.customer} 🧜‍♀️\n`;
 
-  result += '------------------------ \n'; 
-
-  // console.log(result.performances);
-
+  result += '------------------------ \n';  // console.log(result.performances);
   const format = new Intl.NumberFormat('en-US', 
   {
     style: 'currency', currency: 'USD',
     minimumFractionDigits: 2
   }).format;
 
+// CALC
+  function amountFor(aPerformance) {
+  let result = 0;
+ 
+  switch(playFor(aPerformance).type) {
+    case 'tragedy':
+    result = 40000;
+    if(aPerformance.audience > 30) {
+      result += 1000 * (aPerformance.audience - 30); 
+    }
+    break;
+    case 'comedy': 
+    result = 30000;
+    if(aPerformance.audience > 20) {
+      result += 1000 + 500 * (aPerformance.audience - 20); 
+    }
+    result += 300 * aPerformance.audience; 
+    break;
+    default:
+    throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+  }
+  return result; 
+}
+
+// GET Play
+function playFor(aPerformance) {
+  return plays[aPerformance.playID]
+}
   // loop to each performance
   for(let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = 0;
-
-    switch(play.type) {
-      case 'tragedy':
-      thisAmount = 40000;
-      if(perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30); 
-      }
-      break;
-
-      case 'comedy': 
-      thisAmount = 30000;
-      if(perf.audience > 20) {
-        thisAmount += 1000 + 500 * (perf.audience - 20); 
-      }
-      thisAmount += 300 * perf.audience; 
-      break;
-
-      default:
-      throw new Error(`unknown type: ${play.type}`);
-    }
+    // const play = plays[perf.playID];
+    // const play = playFor(perf);
+    let thisAmount = amountFor(perf); 
 
     // add volume volume volumeCredits
     volumeCredits += Math.max(perf.audience - 30, 0); 
 
     // add extra** credit for every 10 comedy attendees
-    if('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
-
+    if('comedy' === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
     // PRINT line for this order
-    result += `${play.name} :: ${format(thisAmount/100)}
+    result += `${playFor(perf).name} :: ${format(thisAmount/100)}
     (${perf.audience} seats) \n`;
     totalAmount += thisAmount;
 
